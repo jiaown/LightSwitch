adce1fdc528669b3c2a779a8c48577e8d3e18f99
jiaown <316231881@qq.com>
2015-08-02 14:33:02 +0800

    finished

--- src/com/jiaown/lightswitch/MainActivity.java
+++ src/com/jiaown/lightswitch/MainActivity.java
@@ -9,14 +9,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 import android.os.Build;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnCheckedChangeListener{
+	
+	private ToggleButton tb;
+	private ImageView img;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//³õÊ¼»¯¿Ø¼þ
+		tb = (ToggleButton) findViewById(R.id.toggleButton1);
+		img = (ImageView) findViewById(R.id.imageView1);
+		
+		tb.setOnCheckedChangeListener(this);
+	}
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		// TODO Auto-generated method stub
+		
+		img.setBackgroundResource(isChecked?R.drawable.on:R.drawable.off);
+		
 	}
 
 
